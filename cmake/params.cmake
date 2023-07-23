7646f5f9171afa224cb4a9aa07efb7fb84d28ba0
Denis Pronin <dannftk@yandex.ru>
2023-07-23 22:03:01 +0300

    give a new project directory 'etc' that holds configuration for etc
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- cmake/params.cmake
+++ cmake/params.cmake
@@ -8,8 +8,6 @@ if (NOT UBLK_CFG_DIR)
     set(UBLK_CFG_DIR "/etc/ublk")
 endif()
 
-set(UBLK_CFG_FILE_PATH_DEFAULT "${UBLK_CFG_DIR}/cfg.json")
-
 if (NOT UBLK_LOG_LEVEL_DEFAULT)
     set(UBLK_LOG_LEVEL_DEFAULT "info")
 endif()
