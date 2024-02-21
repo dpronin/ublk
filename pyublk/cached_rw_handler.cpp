30f0fc11163e177090f66a340cf9c5e88f938bc6
Denis Pronin <dannftk@yandex.ru>
2024-02-21 21:04:10 +0300

    CachedRWHandler::write() small refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/cached_rw_handler.cpp
+++ pyublk/cached_rw_handler.cpp
@@ -53,16 +53,17 @@ ssize_t CachedRWHandler::read(std::span<std::byte> buf,
 
 ssize_t CachedRWHandler::write(std::span<std::byte const> buf,
                                __off64_t offset) noexcept {
-  ssize_t rb{0};
+  ssize_t wb{0};
 
   auto chunk_id{offset / cache_->item_sz()};
   auto chunk_offset{offset % cache_->item_sz()};
 
   while (!buf.empty()) {
-    auto const chunk =
-        buf.subspan(0, std::min(cache_->item_sz() - chunk_offset, buf.size()));
+    auto const chunk{
+        buf.subspan(0, std::min(cache_->item_sz() - chunk_offset, buf.size())),
+    };
 
-    if (auto const res = handler_->write(chunk, offset + rb); res < 0)
+    if (auto const res = handler_->write(chunk, offset + wb); res < 0)
         [[unlikely]] {
       cache_->invalidate(chunk_id);
       return res;
@@ -72,18 +73,18 @@ ssize_t CachedRWHandler::write(std::span<std::byte const> buf,
     auto [cached_chunk, _] = cache_->find_allocate_mutable(chunk_id);
     assert(!cached_chunk.empty());
 
-    auto const from = chunk;
-    auto const to = cached_chunk.subspan(chunk_offset, chunk.size());
+    auto const from{chunk};
+    auto const to{cached_chunk.subspan(chunk_offset, chunk.size())};
 
     algo::copy(from, to);
 
-    rb += chunk.size();
+    wb += chunk.size();
     chunk_offset = 0;
     ++chunk_id;
     buf = buf.subspan(chunk.size());
   }
 
-  return rb;
+  return wb;
 }
 
 } // namespace ublk
