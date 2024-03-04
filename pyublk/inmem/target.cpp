e98bd3a2bcb518e086956887343e9627cb25d7eb
Denis Pronin <dannftk@yandex.ru>
2024-03-04 23:07:50 +0300

    inmem/target.cpp small refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/inmem/target.cpp
+++ pyublk/inmem/target.cpp
@@ -23,9 +23,10 @@ int Target::process(std::shared_ptr<read_query> rq) noexcept {
   if (mem_sz_ < rq->offset() + rq->buf().size()) [[unlikely]]
     return EINVAL;
 
-  auto const from =
-      std::span<std::byte const>{mem_.get() + rq->offset(), rq->buf().size()};
-  auto const to = rq->buf();
+  auto const from{
+      std::span<std::byte const>{mem_.get() + rq->offset(), rq->buf().size()},
+  };
+  auto const to{rq->buf()};
 
   algo::copy(from, to);
 
@@ -38,8 +39,8 @@ int Target::process(std::shared_ptr<write_query> wq) noexcept {
   if (mem_sz_ < wq->offset() + wq->buf().size()) [[unlikely]]
     return EINVAL;
 
-  auto const from = wq->buf();
-  auto const to = std::span{mem_.get() + wq->offset(), wq->buf().size()};
+  auto const from{wq->buf()};
+  auto const to{std::span{mem_.get() + wq->offset(), wq->buf().size()}};
 
   algo::copy(from, to);
 
