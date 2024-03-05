f10a47bb216f58da496c09117f59ea2ee6540d0d
Denis Pronin <dannftk@yandex.ru>
2024-03-05 21:11:18 +0300

    introduce and use bitset_semaphore.hpp
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- utils/bitset_rw_semaphore.hpp
+++ utils/bitset_rw_semaphore.hpp
@@ -1,26 +1,16 @@
 #pragma once
 
-#include <cassert>
 #include <cstdint>
 
-#include <boost/dynamic_bitset/dynamic_bitset.hpp>
+#include "bitset_semaphore.hpp"
 
 namespace ublk {
 
 template <typename Allocator> class bitset_rw_semaphore final {
-private:
-  using state_t = boost::dynamic_bitset<uint64_t, Allocator>;
-
-  void extend(uint64_t nr, state_t &lock_state) {
-    if (nr > lock_state.size())
-      lock_state.resize(nr);
-  }
-
 public:
   explicit bitset_rw_semaphore(uint64_t preallocate_size = 0,
                                Allocator const alloc = {})
-      : rlock_state_(preallocate_size, 0, alloc),
-        wlock_state_(preallocate_size, 0, alloc) {}
+      : rstate_(preallocate_size, alloc), wstate_(preallocate_size, alloc) {}
   ~bitset_rw_semaphore() = default;
 
   bitset_rw_semaphore(bitset_rw_semaphore const &) = delete;
@@ -30,34 +20,18 @@ public:
   bitset_rw_semaphore &operator=(bitset_rw_semaphore &&) = delete;
 
   void extend(uint64_t nr) noexcept {
-    for (auto *p_lock_state : {&rlock_state_, &wlock_state_})
-      extend(nr, *p_lock_state);
+    for (auto *p_state : {&rstate_, &wstate_})
+      p_state->extend(nr);
   }
 
-  bool try_read_lock(uint64_t pos) noexcept {
-    assert(pos < rlock_state_.size());
-    return !rlock_state_.test_set(pos);
-  }
+  bool try_read_lock(uint64_t pos) noexcept { return rstate_.try_lock(pos); }
+  void read_unlock(uint64_t pos) noexcept { rstate_.unlock(pos); }
 
-  void read_unlock(uint64_t pos) noexcept {
-    assert(pos < rlock_state_.size());
-    assert(rlock_state_[pos]);
-    rlock_state_.reset(pos);
-  }
-
-  bool try_write_lock(uint64_t pos) noexcept {
-    assert(pos < wlock_state_.size());
-    return !wlock_state_.test_set(pos);
-  }
-
-  void write_unlock(uint64_t pos) noexcept {
-    assert(pos < wlock_state_.size());
-    assert(wlock_state_[pos]);
-    wlock_state_.reset(pos);
-  }
+  bool try_write_lock(uint64_t pos) noexcept { return wstate_.try_lock(pos); }
+  void write_unlock(uint64_t pos) noexcept { wstate_.unlock(pos); }
 
 private:
-  state_t rlock_state_, wlock_state_;
+  bitset_semaphore<Allocator> rstate_, wstate_;
 };
 
 } // namespace ublk
