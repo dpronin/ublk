951436f0aabc44281b6c67601eb9b71dbf0d7b9f
Denis Pronin <dannftk@yandex.ru>
2024-03-07 21:52:53 +0300

    ut: RAID5 refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- ut/raid5.cpp
+++ ut/raid5.cpp
@@ -47,6 +47,12 @@ TEST_P(RAID5, TestReading) {
     return ut::make_unique_random_bytes(storage_sz);
   });
 
+  std::vector<std::span<std::byte const>> storage_spans{storages.size()};
+  std::ranges::transform(
+      storages, storage_spans.begin(), [storage_sz](auto const &storage) {
+        return std::span<std::byte const>{storage.get(), storage_sz};
+      });
+
   ublk::raid5::Target tgt{param.strip_sz, {hs.begin(), hs.end()}};
 
   auto const parity_full_cycles{param.stripes_nr / hs.size()};
@@ -56,7 +62,7 @@ TEST_P(RAID5, TestReading) {
     EXPECT_CALL(*hs[i], submit(An<std::shared_ptr<read_query>>()))
         .Times((hs.size() - 1) * parity_full_cycles + parity_cycle_rem -
                !(i < (hs.size() - parity_cycle_rem)))
-        .WillRepeatedly(ut::make_inmem_reader({storages[i].get(), storage_sz}));
+        .WillRepeatedly(ut::make_inmem_reader(storage_spans[i]));
   }
 
   auto const buf_sz{(hs.size() - 1) * param.strip_sz * param.stripes_nr};
@@ -78,9 +84,7 @@ TEST_P(RAID5, TestReading) {
     auto const sid = sids_rotated[(off / param.strip_sz) % (hs.size() - 1)];
     auto const soff = stripe_id * param.strip_sz;
     auto const s1{std::as_bytes(buf_span.subspan(off, param.strip_sz))};
-    auto const s2{
-        std::as_bytes(std::span{storages[sid].get() + soff, param.strip_sz}),
-    };
+    auto const s2{storage_spans[sid].subspan(soff, param.strip_sz)};
     EXPECT_TRUE(std::ranges::equal(s1, s2));
   }
 }
@@ -97,11 +101,17 @@ TEST_P(RAID5, TestWriting) {
     return std::make_unique<std::byte[]>(storage_sz);
   });
 
+  std::vector<std::span<std::byte>> storage_spans{storages.size()};
+  std::ranges::transform(
+      storages, storage_spans.begin(), [storage_sz](auto const &storage) {
+        return std::span<std::byte>{storage.get(), storage_sz};
+      });
+
   ublk::raid5::Target tgt{param.strip_sz, {hs.begin(), hs.end()}};
   for (size_t i = 0; i < hs.size(); ++i) {
     EXPECT_CALL(*hs[i], submit(An<std::shared_ptr<write_query>>()))
         .Times(param.stripes_nr)
-        .WillRepeatedly(ut::make_inmem_writer({storages[i].get(), storage_sz}));
+        .WillRepeatedly(ut::make_inmem_writer(storage_spans[i]));
   }
 
   auto const buf_sz{(hs.size() - 1) * param.strip_sz * param.stripes_nr};
@@ -124,7 +134,7 @@ TEST_P(RAID5, TestWriting) {
     auto const soff = stripe_id * param.strip_sz;
     auto const s1{buf_span.subspan(off, param.strip_sz)};
     auto const s2{
-        std::as_bytes(std::span{storages[sid].get() + soff, param.strip_sz}),
+        std::as_bytes(storage_spans[sid].subspan(soff, param.strip_sz)),
     };
     EXPECT_TRUE(std::ranges::equal(s1, s2));
   }
