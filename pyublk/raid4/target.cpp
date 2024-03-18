c3e42a478371bbfbee448daa789c4ee7b9013f36
Denis Pronin <dannftk@yandex.ru>
2024-03-18 18:40:58 +0300

    RAID4: use sml
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/raid4/target.cpp
+++ pyublk/raid4/target.cpp
@@ -3,11 +3,15 @@
 #include <memory>
 #include <utility>
 
+#include <boost/sml.hpp>
+
 #include "raidsp/target.hpp"
 
-namespace ublk::raid4 {
+using namespace ublk;
 
-class Target::impl final : public raidsp::Target {
+namespace {
+
+class r4 final : public raidsp::Target {
 public:
   using raidsp::Target::Target;
 
@@ -16,6 +20,73 @@ private:
                                   [[maybe_unused]]) const noexcept override {
     return hs_.size() - 1;
   }
+};
+
+struct erq {
+  std::shared_ptr<read_query> rq;
+  mutable int r;
+};
+
+struct ewq {
+  std::shared_ptr<write_query> wq;
+  mutable int r;
+};
+
+struct estripecohcheck {
+  uint64_t stripe_id;
+  mutable bool r;
+};
+
+/* clang-format off */
+struct transition_table {
+  auto operator()() noexcept {
+    using namespace boost::sml;
+    return make_transition_table(
+       // online state
+       *"online"_s + event<erq> [ ([](erq const &e, r4& r){ e.r = r.process(std::move(e.rq)); return 0 == e.r; }) ]
+      , "online"_s + event<erq> = "offline"_s
+      , "online"_s + event<ewq> [ ([](ewq const &e, r4& r){ e.r = r.process(std::move(e.wq)); return 0 == e.r; }) ]
+      , "online"_s + event<ewq> = "offline"_s
+      , "online"_s + event<estripecohcheck> / [](estripecohcheck const &e, r4 const& r) { e.r = r.is_stripe_parity_coherent(e.stripe_id); }
+       // offline state
+      , "offline"_s + event<erq> / [](erq const &e) { e.r = EIO; }
+      , "offline"_s + event<ewq> / [](ewq const &e) { e.r = EIO; }
+      , "offline"_s + event<estripecohcheck> / [](estripecohcheck const &e) { e.r = false; }
+    );
+  }
+};
+/* clang-format on */
+
+} // namespace
+
+namespace ublk::raid4 {
+
+class Target::impl final {
+public:
+  explicit impl(uint64_t strip_sz, std::vector<std::shared_ptr<IRWHandler>> hs)
+      : r4_(strip_sz, std::move(hs)), fsm_(r4_) {}
+
+  int process(std::shared_ptr<read_query> rq) noexcept {
+    erq e{.rq = std::move(rq), .r = 0};
+    fsm_.process_event(e);
+    return e.r;
+  }
+
+  int process(std::shared_ptr<write_query> wq) noexcept {
+    ewq e{.wq = std::move(wq), .r = 0};
+    fsm_.process_event(e);
+    return e.r;
+  }
+
+  bool is_stripe_parity_coherent(uint64_t stripe_id) const noexcept {
+    estripecohcheck e{.stripe_id = stripe_id, .r = false};
+    fsm_.process_event(e);
+    return e.r;
+  }
+
+private:
+  r4 r4_;
+  mutable boost::sml::sm<transition_table> fsm_;
 };
 
 Target::Target(uint64_t strip_sz, std::vector<std::shared_ptr<IRWHandler>> hs)
