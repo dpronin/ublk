cdf0ade23bb7015891c519ffae5c58bfef5adf1d
Denis Pronin <dannftk@yandex.ru>
2024-03-18 17:51:51 +0300

    RAID5's target is turned into PIMPL-based
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/raid5/target.cpp
+++ pyublk/raid5/target.cpp
@@ -1,9 +1,39 @@
 #include "target.hpp"
 
+#include <memory>
+#include <utility>
+
+#include "raidsp/target.hpp"
+
 namespace ublk::raid5 {
 
-uint64_t Target::stripe_id_to_parity_id(uint64_t stripe_id) const noexcept {
-  return hs_.size() - (stripe_id % hs_.size()) - 1;
+class Target::impl final : public raidsp::Target {
+public:
+  using raidsp::Target::Target;
+
+private:
+  uint64_t stripe_id_to_parity_id(uint64_t stripe_id) const noexcept override {
+    return hs_.size() - (stripe_id % hs_.size()) - 1;
+  }
+};
+
+Target::Target(uint64_t strip_sz, std::vector<std::shared_ptr<IRWHandler>> hs)
+    : pimpl_(std::make_unique<impl>(strip_sz, std::move(hs))) {}
+
+Target::~Target() noexcept = default;
+
+Target::Target(Target &&) noexcept = default;
+Target &Target::operator=(Target &&) noexcept = default;
+
+bool Target::is_stripe_parity_coherent(uint64_t stripe_id) const noexcept {
+  return pimpl_->is_stripe_parity_coherent(stripe_id);
+}
+
+int Target::process(std::shared_ptr<read_query> rq) noexcept {
+  return pimpl_->process(std::move(rq));
+}
+int Target::process(std::shared_ptr<write_query> wq) noexcept {
+  return pimpl_->process(std::move(wq));
 }
 
 } // namespace ublk::raid5
--- pyublk/raid5/target.hpp
+++ pyublk/raid5/target.hpp
@@ -1,15 +1,34 @@
 #pragma once
 
-#include "raidsp/target.hpp"
+#include <cstdint>
+
+#include <memory>
+#include <vector>
+
+#include "rw_handler_interface.hpp"
 
 namespace ublk::raid5 {
 
-class Target final : public raidsp::Target {
+class Target final {
 public:
-  using raidsp::Target::Target;
+  explicit Target(uint64_t strip_sz,
+                  std::vector<std::shared_ptr<IRWHandler>> hs);
+  ~Target() noexcept;
 
-protected:
-  uint64_t stripe_id_to_parity_id(uint64_t stripe_id) const noexcept override;
+  Target(Target const &) = delete;
+  Target &operator=(Target const &) = delete;
+
+  Target(Target &&) noexcept;
+  Target &operator=(Target &&) noexcept;
+
+  bool is_stripe_parity_coherent(uint64_t stripe_id) const noexcept;
+
+  int process(std::shared_ptr<read_query> rq) noexcept;
+  int process(std::shared_ptr<write_query> wq) noexcept;
+
+private:
+  class impl;
+  std::unique_ptr<impl> pimpl_;
 };
 
 } // namespace ublk::raid5
