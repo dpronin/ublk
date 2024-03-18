d20a25829c02ffad955b96b9d9c8e26d3e158973
Denis Pronin <dannftk@yandex.ru>
2024-03-18 22:22:38 +0300

    bench_mem_utility.hpp: use strided view
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- bench/bench_mem_utility.hpp
+++ bench/bench_mem_utility.hpp
@@ -1,20 +1,22 @@
 #include <cstddef>
 
+#include <ranges>
+
 #include "mm/mem.hpp"
 #include "mm/mem_types.hpp"
 
 #include "utils/page.hpp"
-#include "utils/utility.hpp"
 
 namespace ublk::bench {
 
 inline auto make_buffer(size_t sz) {
   auto const page_size = get_page_size();
   auto buf = make_unique_bytes(mm::alloc_mode_mmap{}, sz);
+  auto const buf_span{std::span{buf.get(), sz}};
   /* initialize a first byte of each page to prevent page faults while
    * benchmarking */
-  for (size_t i = 0, n = div_round_up(sz, page_size); i < n; ++i)
-    buf[i * page_size] = static_cast<std::byte>(std::rand());
+  for (auto &b : buf_span | std::views::stride(page_size))
+    b = static_cast<std::byte>(std::rand());
   return buf;
 }
 
