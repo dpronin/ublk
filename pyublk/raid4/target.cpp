fb8d5c1efa9f54d304317d76310a7cd2f69e4b28
Denis Pronin <dannftk@yandex.ru>
2024-03-23 21:20:40 +0300

    update RAID4 and RAID5 API of targets
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/raid4/target.cpp
+++ pyublk/raid4/target.cpp
@@ -12,9 +12,13 @@ namespace ublk::raid4 {
 class Target::impl final {
 public:
   explicit impl(uint64_t strip_sz, std::vector<std::shared_ptr<IRWHandler>> hs)
-      : target_(strip_sz, hs, [strips_nr = hs.size()](uint64_t stripe_id [[maybe_unused]]) {
-          return strips_nr - 1;
-        }) {}
+      : target_(strip_sz, hs,
+                [strips_per_stripe_nr =
+                     hs.size()](uint64_t stripe_id [[maybe_unused]]) {
+                  return strips_per_stripe_nr - 1;
+                }) {}
+
+  std::string state() const { return target_.state(); }
 
   int process(std::shared_ptr<read_query> rq) noexcept {
     return target_.process(std::move(rq));
@@ -39,6 +43,8 @@ Target::~Target() noexcept = default;
 
 Target::Target(Target &&) noexcept = default;
 Target &Target::operator=(Target &&) noexcept = default;
+
+std::string Target::state() const { return pimpl_->state(); }
 
 bool Target::is_stripe_parity_coherent(uint64_t stripe_id) const noexcept {
   return pimpl_->is_stripe_parity_coherent(stripe_id);
--- pyublk/raid4/target.hpp
+++ pyublk/raid4/target.hpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 
 #include <memory>
+#include <ranges>
+#include <string>
 #include <vector>
 
 #include "rw_handler_interface.hpp"
@@ -16,6 +18,10 @@ class Target final {
 public:
   explicit Target(uint64_t strip_sz,
                   std::vector<std::shared_ptr<IRWHandler>> hs);
+
+  explicit Target(uint64_t strip_sz, std::ranges::input_range auto &&hs)
+      : Target(strip_sz, {std::ranges::begin(hs), std::ranges::end(hs)}) {}
+
   ~Target() noexcept;
 
   Target(Target const &) = delete;
@@ -23,6 +29,8 @@ public:
 
   Target(Target &&) noexcept;
   Target &operator=(Target &&) noexcept;
+
+  std::string state() const;
 
   bool is_stripe_parity_coherent(uint64_t stripe_id) const noexcept;
 
