dec22bfd6b7bc013578ef49ef4827626aebdedeb
Denis Pronin <dannftk@yandex.ru>
2024-03-23 22:26:48 +0300

    parity.cpp: modernize initialization of variables according to new C++
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- pyublk/raidsp/parity.cpp
+++ pyublk/raidsp/parity.cpp
@@ -13,18 +13,22 @@ void parity_to(std::span<std::byte const> data, std::span<std::byte> parity,
                size_t parity_start_offset /* = 0*/) noexcept {
   assert(is_multiple_of(parity_start_offset, sizeof(uint64_t)));
 
-  auto data_u64 = to_span_of<uint64_t const>(data);
-  auto const parity_u64 = to_span_of<uint64_t>(parity);
+  auto data_u64{to_span_of<uint64_t const>(data)};
+  auto const parity_u64{to_span_of<uint64_t>(parity)};
 
   parity_start_offset = parity_start_offset % parity.size();
 
-  if (auto const parity_u64_offset = parity_start_offset / sizeof(uint64_t)) {
-    auto const chunk_u64_len =
-        std::min(data_u64.size(), parity_u64.size() - parity_u64_offset);
-    auto const parity_u64_chunk =
-        parity_u64.subspan(parity_u64_offset, chunk_u64_len);
-    auto const data_u64_chunk = data_u64.subspan(0, chunk_u64_len);
+  if (auto const parity_u64_offset{parity_start_offset / sizeof(uint64_t)}) {
+    auto const chunk_u64_len{
+        std::min(data_u64.size(), parity_u64.size() - parity_u64_offset),
+    };
+    auto const parity_u64_chunk{
+        parity_u64.subspan(parity_u64_offset, chunk_u64_len),
+    };
+    auto const data_u64_chunk{data_u64.subspan(0, chunk_u64_len)};
+
     math::xor_to(data_u64_chunk, parity_u64_chunk);
+
     data_u64 = data_u64.subspan(data_u64_chunk.size());
   }
 
