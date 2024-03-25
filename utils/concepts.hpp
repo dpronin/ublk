e999931dcab1e4b7d7c252666fa9808919e5efe1
Denis Pronin <dannftk@yandex.ru>
2024-03-25 08:47:11 +0300

    add one new concept to utils/concepts.hpp
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- utils/concepts.hpp
+++ utils/concepts.hpp
@@ -16,4 +16,7 @@ concept is_byte = std::is_same_v<std::remove_const_t<T>, std::byte>;
 template <typename T>
 concept is_trivial = std::is_trivial_v<T>;
 
+template <typename T>
+concept is_trivially_copyable = std::is_trivially_copyable_v<T>;
+
 } // namespace ublk
