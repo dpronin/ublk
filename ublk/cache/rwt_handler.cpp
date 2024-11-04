fb4651fe4b6ae4f9d6e5783ffbcebf4ac1b36c45
Denis Pronin <dannftk@yandex.ru>
2024-11-04 16:17:47 +0300

    WIP

--- ublk/cache/rwt_handler.cpp
+++ ublk/cache/rwt_handler.cpp
@@ -7,8 +7,6 @@
 
 #include <gsl/assert>
 
-#include "mm/generic_allocators.hpp"
-
 #include "utils/algo.hpp"
 
 namespace ublk::cache {
@@ -18,8 +16,7 @@ RWTHandler::RWTHandler(
     std::shared_ptr<IRWHandler> handler,
     std::shared_ptr<mm::mem_chunk_pool> mem_chunk_pool) noexcept
     : RWIHandler(std::move(cache), std::move(handler),
-                 std::move(mem_chunk_pool)),
-      chunk_w_locker_(0, mm::allocator::cache_line_aligned<uint64_t>::value) {}
+                 std::move(mem_chunk_pool)) {}
 
 int RWTHandler::process(std::shared_ptr<write_query> wq) noexcept {
   Expects(wq);
@@ -91,37 +88,77 @@ int RWTHandler::submit(std::shared_ptr<write_query> wq) noexcept {
   Expects(wq);
   Expects(!wq->buf().empty());
 
-  chunk_w_locker_.extend(
-      div_round_up(wq->offset() + wq->buf().size(), cache_->item_sz()));
+  size_t wb{0};
 
   auto chunk_id{wq->offset() / cache_->item_sz()};
-  auto chunk_offset{wq->offset() % cache_->item_sz()};
 
-  for (size_t wb{0}; wb < wq->buf().size(); ++chunk_id, chunk_offset = 0) {
+  if (auto const chunk_off{wq->offset() % cache_->item_sz()}; chunk_off > 0) {
     auto const chunk_sz{
-        std::min(cache_->item_sz() - chunk_offset, wq->buf().size() - wb),
+        std::min(cache_->item_sz() - chunk_off, wq->buf().size()),
+    };
+
+    auto const chunk{wq->buf().subspan(0, chunk_sz)};
+
+    if (auto cached_chunk = cache_->find_mutable(chunk_id);
+        !cached_chunk.empty()) {
+      assert(!(chunk_off + chunk.size() > cached_chunk.size()));
+      auto const from{chunk};
+      auto const to{cached_chunk.subspan(chunk_off, chunk.size())};
+      algo::copy(from, to);
+    } else {
+      auto mem_chunk = mem_chunk_pool_->get();
+      assert(mem_chunk);
+
+      auto mem_chunk_span = mem_chunk_pool_->chunk_view(mem_chunk);
+      auto rmwq = read_query::create(
+          mem_chunk_span, chunk_id * cache_->item_sz(),
+          [this, wq = wq->subquery(0, chunk.size(), wq->offset(), wq), chunk_id,
+           mem_chunk_holder = std::make_shared<decltype(mem_chunk)>(
+               std::move(mem_chunk))](read_query const &rmwq) mutable {
+            if (rmwq.err()) [[unlikely]] {
+              wq->set_err(rmwq.err());
+              return;
+            }
+
+            cache_->update({chunk_id, std::move(*mem_chunk_holder.get())});
+            ++last_wq_done_seq_;
+
+            if (auto const r{submit(wq)}) {
+              wq->set_err(r);
+              return;
+            }
+          });
+      if (auto const r{handler_->submit(std::move(rmwq))}) [[unlikely]] {
+        return r;
+      }
+    }
+
+    ++chunk_id;
+    wb += chunk.size();
+  }
+
+  for (; wb < wq->buf().size(); ++chunk_id) {
+    auto const chunk_sz{
+        std::min(cache_->item_sz(), wq->buf().size() - wb),
     };
 
     auto chunk_wq_completer{
         [this, chunk_id, wq](write_query const &chunk_wq) {
-          for (bool finish = false; !finish;) {
-            if (auto next_wq_it = std::ranges::find(
-                    wqs_pending_, chunk_id,
-                    [](auto const &wq_pend) { return wq_pend.first; });
-                next_wq_it != wqs_pending_.end()) {
-              finish = 0 == process(next_wq_it->second);
-              std::iter_swap(next_wq_it, wqs_pending_.end() - 1);
-              wqs_pending_.pop_back();
-            } else {
-              chunk_w_locker_.unlock(chunk_id);
-              finish = true;
-            }
+          auto wq_it{wqs_pending_.find(chunk_id)};
+          assert(wqs_pending_.end() != wq_it);
+
+          bool submitted{false};
+          while (!wq_it->second.empty() && !submitted) {
+            submitted = 0 == process(wq_it->second.front());
+            wq_it->second.pop();
           }
+
+          if (!submitted)
+            wqs_pending_.erase(wq_it);
 
           if (chunk_wq.err()) [[unlikely]] {
             cache_->invalidate(chunk_id);
             wq->set_err(chunk_wq.err());
-            return;
           }
         },
     };
@@ -131,9 +168,11 @@ int RWTHandler::submit(std::shared_ptr<write_query> wq) noexcept {
                      std::move(chunk_wq_completer)),
     };
 
-    if (!chunk_w_locker_.try_lock(chunk_id)) [[unlikely]] {
-      wqs_pending_.emplace_back(chunk_id, std::move(chunk_wq));
+    if (auto wq_it = wqs_pending_.find(chunk_id); wqs_pending_.end() != wq_it) {
+      wq_it->second.push(std::move(chunk_wq));
     } else if (auto const res{process(std::move(chunk_wq))}) [[unlikely]] {
+      wqs_pending_.emplace_hint(wq_it, chunk_id,
+                                std::queue<std::shared_ptr<write_query>>{});
       return res;
     }
 
